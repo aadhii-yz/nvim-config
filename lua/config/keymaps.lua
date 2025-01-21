8325b107fd22a6e18a626f534b95a003d6fa3049
Adithya <aadhii.yz@gmail.com>
2025-01-21 22:40:50 +0530

    Basic LazyVim config.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- # NOTES :
+-- Delete an existing keymap with `vim.keymap.del`
+-- Add a keymap with `vim.keymap.set`
+
+-- vim.keymap.set("i", "jj", "<ESC>")
+-- vim.keymap.set("v", "<leader>y", '"+y', opts)
