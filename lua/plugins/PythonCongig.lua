2d99ec39a0a763d3a2002ba82bae0fa6354747a0
Adithya <aadhii.yz@gmail.com>
2025-03-26 15:09:33 +0530

    Added PythonConfig.

---
+++ lua/plugins/PythonCongig.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "pyright" }) -- LSP for Python
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				pyright = {}, -- Enable Pyright LSP
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "python" }) -- Treesitter for Python
+		end,
+	},
+}
