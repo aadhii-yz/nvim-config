93809a14898475fe328fb3a6b9a2e16d3e09ba79
Adithya <aadhii.yz@gmail.com>
2025-03-26 15:09:01 +0530

    Modified GoConfig due to some errors.

--- lua/plugins/GoConfig.lua
+++ lua/plugins/GoConfig.lua
@@ -1,40 +1,85 @@
 return {
-  -- Go LSP and Tools
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        gopls = {
-          cmd = { "gopls" },
-          filetypes = { "go", "gomod", "gowork", "gotmpl" },
-          root_dir = require("lspconfig.util").root_pattern("go.work", "go.mod", ".git"),
-          settings = {
-            gopls = {
-              analyses = {
-                unusedparams = true,
-                shadow = true,
-              },
-              staticcheck = true,
-            },
-          },
-        },
-      },
-    },
-  },
+	-- Go LSP and tools
 
-  -- Install Mason packages for Go
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "gopls",          -- LSP server for Go
-        "golangci-lint",  -- Linter
-        "delve",          -- Debugger
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				gopls = {
+					settings = {
+						gopls = {
+							gofumpt = true,
+							codelenses = {
+								gc_details = false,
+								generate = true,
+								regenerate_cgo = true,
+								run_govulncheck = true,
+								test = true,
+								tidy = true,
+								upgrade_dependency = true,
+								vendor = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							analyses = {
+								nilness = true,
+								unusedparams = true,
+								unusedwrite = true,
+								useany = true,
+							},
+							usePlaceholders = true,
+							completeUnimported = true,
+							staticcheck = true,
+							directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+							semanticTokens = true,
+						},
+					},
+				},
+			},
+			setup = {
+				gopls = function(_, opts)
+					-- workaround for gopls not supporting semanticTokensProvider
+					-- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+					LazyVim.lsp.on_attach(function(client, _)
+						if not client.server_capabilities.semanticTokensProvider then
+							local semantic = client.config.capabilities.textDocument.semanticTokens
+							client.server_capabilities.semanticTokensProvider = {
+								full = true,
+								legend = {
+									tokenTypes = semantic.tokenTypes,
+									tokenModifiers = semantic.tokenModifiers,
+								},
+								range = true,
+							}
+						end
+					end, "gopls")
+					-- end workaround
+				end,
+			},
+		},
+	},
 
-  -- Debugging with DAP
+	-- Install Mason packages for Go
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"gopls", -- LSP server for Go
+				"golangci-lint", -- Linter
+				"delve", -- Debugger
+			},
+		},
+	},
+
+	-- Debugging with DAP
+	--[[
   {
     "mfussenegger/nvim-dap",
     opts = function(_, opts)
@@ -64,11 +109,12 @@ return {
       return opts
     end,
   },
+  ]]
 
-  -- Add Go tools
-  {
-    "ray-x/go.nvim",
-    dependencies = { "ray-x/guihua.lua" },
-    config = true,
-  },
+	-- Add Go tools
+	{
+		"ray-x/go.nvim",
+		dependencies = { "ray-x/guihua.lua" },
+		config = true,
+	},
 }
